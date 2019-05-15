41ec7378665bd1050e0b84d091be92e22ddd7725
Nikos Konstantakis <nikonst@sch.gr>
2019-05-15 12:29:41 +0300

    New Line in the End

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,8 +2,8 @@ class HelloWorld {
     public static void main(String[]s) {
         System.out.println("Hello World from Java!");
         for(int i = 0; i < 5; i++) {
-            System.out.print("i = " + i);
+            System.out.print(" i = " + i);
         }
-        
+        System.out.println();
     }
 }
