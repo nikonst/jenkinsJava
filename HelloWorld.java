6e8cd6c0ab637bdad87e78dfe3920cbe7700e68b
Nikos Konstantakis <nikonst@sch.gr>
2019-05-15 12:16:19 +0300

    Loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,9 @@
 class HelloWorld {
     public static void main(String[]s) {
         System.out.println("Hello World from Java!");
+        for(int i = 0; i < 5; i++) {
+            System.out.print("i = " + i);
+        }
         
     }
 }
